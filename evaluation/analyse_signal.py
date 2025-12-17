2b8adf93210586594ef1654ddf1950c99a94db5a
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2025-12-17 15:13:44 +0000

    Add correlation analysis of signal

--- evaluation/analyse_signal.py
+++ evaluation/analyse_signal.py
@@ -1,13 +1,15 @@
 import os
 import pathlib
 
+import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.stats import spearmanr
 
 from evaluation.compare_types import plot_binary_and_continuous_cases
 from evaluation.compile_prediction_scores import read_all_results
 import seaborn as sns
 
-from evaluation.do_ttests_for_results import ttests
+from evaluation.do_ttests_for_results import ttests, hochberg_correction
 from evaluation.helper_functions import bin_model_names, cont_model_names, rename_models_and_ev_models
 
 
@@ -109,38 +111,52 @@ def plot_signal_performance_two_models(df, model1, model2, bin_or_cont, out_dir,
     plt.savefig(os.path.join(out_dir, f'{model1}_{model2}_{str(drop_outlier)}.jpg'), dpi=300)
     plt.close()
 
-
 def main():
     out_dir = 'signal_analysis'
     pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
 
     bin_df, cont_df = read_all_results()
     # # plot signal distribution
-    # sns.displot(bin_df.reset_index(drop=True), x='Signal', kind='kde')
-    # plt.savefig(os.path.join(out_dir, f'D.jpg'), dpi=300)
-    # plt.close()
-    #
-    # sns.displot(cont_df, x='Signal', kind='kde')
-    # plt.savefig(os.path.join(out_dir, f'lambda.jpg'), dpi=300)
-    # plt.close()
-    #
-    # ## plot performance against signal for each model
-    # for model in bin_model_names:
-    #     plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model))
-    #     plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model), drop_outlier=True)
-    #
-    #     if model != 'corHMM':
-    #         # plot performance against signal for corHMM and a ML model
-    #         plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'))
-    #         plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'), drop_outlier=True)
-    #
-    # for model in cont_model_names:
-    #     plot_signal_performance(cont_df, model, 'continuous', os.path.join(out_dir, 'continuous models', model))
-    #
-    #     if model != 'phylopars':
-    #         # plot performance against signal for corHMM and a ML model
-    #         plot_signal_performance_two_models(cont_df, 'phylopars', model, 'continuous', os.path.join(out_dir, 'models vs phylopars'))
-    #
+    sns.displot(bin_df.reset_index(drop=True), x='Signal', kind='kde')
+    plt.savefig(os.path.join(out_dir, f'D.jpg'), dpi=300)
+    plt.close()
+
+    sns.displot(cont_df, x='Signal', kind='kde')
+    plt.savefig(os.path.join(out_dir, f'lambda.jpg'), dpi=300)
+    plt.close()
+
+    # Calculate correlations
+    spearman_values = []
+    spearman_indices = []
+    for model in bin_model_names:
+        model_correlation = spearmanr(bin_df['Signal'], bin_df[model], nan_policy='omit')
+        spearman_values.append([model_correlation.statistic, model_correlation.pvalue])
+        spearman_indices.append(f'{model}_bin')
+    for model in cont_model_names:
+        model_correlation = spearmanr(cont_df['Signal'], cont_df[model], nan_policy='omit')
+        spearman_values.append([model_correlation.statistic, model_correlation.pvalue])
+        spearman_indices.append(f'{model}_cont')
+    spearmanr_df = pd.DataFrame(spearman_values, index=spearman_indices,
+                                columns=['spearman_values', 'pvalues'])
+    h_sp_df = hochberg_correction(spearmanr_df, 'pvalues')
+    h_sp_df.to_csv(os.path.join(out_dir, 'spearman_correlations.csv'))
+    ## plot performance against signal for each model
+    for model in bin_model_names:
+        plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model))
+        plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model), drop_outlier=True)
+
+        if model != 'corHMM':
+            # plot performance against signal for corHMM and a ML model
+            plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'))
+            plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'), drop_outlier=True)
+
+    for model in cont_model_names:
+        plot_signal_performance(cont_df, model, 'continuous', os.path.join(out_dir, 'continuous models', model))
+
+        if model != 'phylopars':
+            # plot performance against signal for corHMM and a ML model
+            plot_signal_performance_two_models(cont_df, 'phylopars', model, 'continuous', os.path.join(out_dir, 'models vs phylopars'))
+
     # #  phylogenetic imputation may perform poorly when lambda is less than 0.6 (Molina-Venegas et al., 2018)
     # ## compare models with signal above 0.6 and signal below 0.6
     binary_above_06_df = bin_df[bin_df['Signal'] <= 0.5]
